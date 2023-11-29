4b7d46bc3a2eb6a68e0f19eb27a2dfc0b7f1c323
kangjiwon95 <148264109+kangjiwon95@users.noreply.github.com>
2023-11-30 00:07:56 +0900

    Add files via upload

---
+++ 02.LinkedList/LinkedList.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Diagnostics;
+using System.Collections;
+
+namespace _02.LinkedList
+{
+
+    // 양방향 리스트
+    public class MyLinkedListNode<T>
+    {
+        public MyLinkedListNode<T> prev;
+        public MyLinkedListNode<T> next;
+        public T item;
+
+        public MyLinkedListNode(T Item)
+        {
+            
+            this.prev = null;
+            this.next = null;
+            this.Item = item;
+        }
+
+        public  MyLinkedListNode(MyLinkedListNode<T> prev, MyLinkedListNode<T> next, T Item)
+        {
+            this.prev = prev;
+            this.next = next;
+            this.Item = item;
+        }
+
+        public MyLinkedListNode<T> Prev { get { return prev; } }
+        public MyLinkedListNode<T> Next { get { return next; } }
+        public T Item { get { return item; } set { Item = value; } }
+    }
+
+    
+   
+    public class MyLinkedList<T>
+    {
+        private MyLinkedListNode<T> head;
+        private MyLinkedListNode<T> tail;
+        private int count;
+        
+        public MyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+            this.count = 0;
+        }
+
+        public MyLinkedListNode<T> AddFirst(T value)
+        {
+            MyLinkedListNode<T> newNode = new MyLinkedListNode<T>( value );
+            if (count > 0 ) // 다른노드가 있었던 상황
+            {
+                newNode.next = this.head;
+                this.head = newNode;
+                head = newNode;
+            }  
+            else // 처음 추가하는 상황 == 노드가 하나도 없던 상황
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            count++;
+
+            return newNode;
+        }
+
+        public MyLinkedListNode<T> AddLast(T value)
+        {
+            MyLinkedListNode<T> newNode = new MyLinkedListNode<T> (value);
+            if (count > 0 ) // 다른노드가 있었던 상황
+            {
+                newNode.next = this.tail;
+                this.tail = newNode;
+                tail = newNode;
+            }
+            else  // 처음 추가하는 상황 == 노드가 하나도 없던 상황
+            {
+                head = newNode;
+                tail = newNode;
+            }
+                  
+            count++;
+
+            return newNode;
+        }
+
+        public MyLinkedListNode<T> Find(T value)
+        {
+            MyLinkedListNode<T> current = head;
+            EqualityComparer<T> comparer = EqualityComparer<T>.Default;
+            
+            while (current != null)
+            {
+                if (comparer.Equals(current.Item,value))
+                {
+                    return current;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+            return null;
+        }
+
+        public bool Remove(T value)
+        {
+            MyLinkedListNode<T> findNode = Find(value);
+            if ( findNode == null )  // 못찾았음 == 연결리스트에 해당 데이터가 없었음 
+            { 
+                return false;
+            }
+            else
+            {
+                if( findNode.prev != null )  // 이전노드가 있었을 때
+                {
+                    findNode.prev.next = findNode.next; // 이전노드의 다음노드를 찾은 노드 다음으로
+                }
+                else // 이전노드가 없었을 때 == 가장 앞 노드였을 때
+                {
+                    head = findNode.next;   // 새로운 헤드를 찾은 노드 다음으로
+                }
+                if (findNode.next != null)  // 다음노드가 있었을 때
+                {
+                    findNode.next.prev = findNode.prev; // 다음노드의 이전노드를 찾은 노드 이전으로
+                }
+                else // 다음노드가 없었을 때 == 가장 뒷 노드였을 때
+                {
+                    tail = findNode.prev;
+                }
+                count--;
+
+                return true;   // 그림판으로 그려서 설명 해보기
+            }
+        }
+
+        public MyLinkedListNode<T> AddBefore(MyLinkedListNode<T> node, T value)
+        {
+            MyLinkedListNode<T> newNode = new MyLinkedListNode<T>(value);
+            if (node != head)
+            {
+                node.prev.next = newNode;
+                newNode.prev = node.prev;
+            }
+            else
+            {
+                head = newNode;
+            }
+            
+            newNode.next = node;
+            node.prev = newNode;
+
+            count++;
+
+            return newNode;
+        }
+
+        public MyLinkedListNode<T> AddAfter(MyLinkedListNode<T> node, T value)
+        {
+            MyLinkedListNode<T> newNode = new MyLinkedListNode<T> ( value );
+            if (node != tail)
+            {
+               node.next.prev = newNode;
+                newNode.next = node.next;
+            }
+            else
+            {
+                tail = newNode;
+            }
+
+            newNode.prev = node;
+            node.next = newNode;
+
+            count++;
+
+            return newNode;
+        }
+
+        public void Remove(MyLinkedListNode<T> node)
+        {
+            if (node.prev != null)
+            {
+                node.prev.next = node.next;
+            }
+            else
+            {
+                head = node.next;
+            }
+            if (node.next != null)
+            {
+                node.next.prev = node.prev;
+            }
+            else
+            {
+                tail = node.prev;
+            }
+
+            count--;
+        }
+        public MyLinkedListNode<T> First { get { return head; } }
+        public MyLinkedListNode<T> Last { get { return tail; } }
+        public int Count { get { return count; } }
+    }
+}
---
+++ 02.LinkedList/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace _02.LinkedList
+{
+    internal class Program
+    {
+        /*면접 질문 이론만*/
+        /******************************************************
+		 * 연결 리스트 (Linked List)
+		 * 
+		 * 데이터를 포함하는 노드들을 연결식으로 만든 자료구조
+		 * 노드는 데이터와 이전/다음 노드 객체를 참조하고 있음
+		 * 노드가 메모리에 연속적으로 배치되지 않고 이전/다음노드의 위치를 확인
+		 ******************************************************/
+
+        //연결 리스트 안쓰는 이유
+        // 1. 삽입 ,삭제에서 이득을 못본다
+        // 2. 추가 삭제 과정마다 쓰레기값(가비지컬렉터)가 많아 실사용 어렵다
+        // 3. 캐시 메모리 적중률이 낮아서 속도면에서 더 느려진다.
+
+
+        // 리스트 종류
+        // 1 단반향 (Singly) 
+        // 2 양방향 (Doubly) 
+        // 3 환형 (Circuar) 
+
+        //   양방향 구조
+        // <연결 리스트 사용 사용>
+        public static void LinkedList()
+        {
+            LinkedList<string> linkedList = new LinkedList<string>();
+
+            // 연결리스트 요소 삽입
+            linkedList.AddFirst("0번 앞데이터");
+            linkedList.AddFirst("1번 앞데이터");
+            linkedList.AddLast("2번 뒤데이터");
+            linkedList.AddLast("3번 뒤데이터");
+
+            // 연결리스트 요소 삭제
+            linkedList.Remove("1번 앞데이터");
+
+            // 연결리스트 요소 탐색
+            LinkedListNode<string> findNode = linkedList.Find("0번 앞데이터");
+
+            // 연결리스트 노드를 통한 노드 참조
+            LinkedListNode<string> prevNode = findNode.Previous; // 이전 노드 확인
+            LinkedListNode<string> nextNode = findNode.Next;     // 다음 노드 확인
+
+            // 연결리스트 노드를 통한 노드 삽입
+            linkedList.AddBefore(findNode, "찾은 노드 앞데이터");
+            linkedList.AddAfter(findNode, "찾은 노드 뒤데이터");
+
+            // 연결리스트 노드를 통한 삭제
+            linkedList.Remove(findNode);  // 검색한 노드 삭제
+
+        }
+
+        // <LinkedList의 시간복잡도>
+        // 접근		탐색		삽입		삭제
+        // O(N)		O(N)	O(1)	O(1)
+
+        // llinkedList[20]
+        // 연결리스트는 index가 없다
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(); 
+        }
+
+    }
+}
+    
